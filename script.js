5a81d29494582e15e640dbd3de2ddef90f73f5ce
JumanaAmr <jumana.amr@ocsbstudent.ca>
2021-02-23 17:09:19 +0000

    Trial 10

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function updateForm () {
+function updateForm(shape) {
   document.getElementById('id-inputs-area').hidden = true;
   document.getElementById('id-inputs-circumference').hidden = true;
   document.getElementById('id-inputs-both').hidden = true;
@@ -7,39 +7,39 @@ function updateForm () {
   let shape = document.getElementById('id-shapes').value;
   switch (shape) {
     case 'area':
-    document.getElementById('id-inputs-area').hidden = false;
-    break;
+      document.getElementById('id-inputs-area').hidden = false;
+      break;
     case 'circumference':
-    document.getElementById('id-inputs-circumference').hidden = false;
-    break;
+      document.getElementById('id-inputs-circumference').hidden = false;
+      break;
     case 'both':
-    document.getElementById('id-inputs-both').hidden = false;
-    break;
+      document.getElementById('id-inputs-both').hidden = false;
+      break;
   }
 }
-function calculateAreacalculateCircumfrance () {
+function calculateAreacalculateCircumfrance (diameter) {
   let shape = document.getElementById('id-shapes').value;
   let area = 0;
   let circumference = 0;
   let diameter = document.getElementById('id-diameter').value;
   switch (shape) {
     case 'area':
-    document.getElementById('id-output-area').hidden = false;
-    area = Math.PI * 1 / 4 * diameter * diameter;
-    document.getElementById('id-output-area').innerHTML = 'Area = ' + area;
-    break;
+      document.getElementById('id-output-area').hidden = false;
+      area = Math.PI * 1 / 4 * diameter * diameter;
+      document.getElementById('id-output-area').innerHTML = 'Area = ' + area;
+      break;
     case 'circumference':
-    document.getElementById('id-output-circumference').hidden = false;
+      document.getElementById('id-output-circumference').hidden = false;
     circumference = Math.PI * diameter;
-    document.getElementById('id-output-circumference').innerHTML = 'Circumference = ' + circumference;
+      document.getElementById('id-output-circumference').innerHTML = 'Circumference = ' + circumference;
     break;
     case 'both':
-    document.getElementById('id-output-circumference').hidden = false;
-    document.getElementById('id-output-area').hidden = false;
-    area = Math.PI * 1 / 4 * diameter * diameter;
-    circumference = Math.PI * diameter;
-    document.getElementById('id-output-circumference').innerHTML = 'Circumference = ' + circumference;
-    document.getElementById('id-output-area').innerHTML = 'Area = ' + area;
-    break;
+      document.getElementById('id-output-circumference').hidden = false;
+      document.getElementById('id-output-area').hidden = false;
+      area = Math.PI * 1 / 4 * diameter * diameter;
+      circumference = Math.PI * diameter;
+      document.getElementById('id-output-circumference').innerHTML = 'Circumference = ' + circumference;
+      document.getElementById('id-output-area').innerHTML = 'Area = ' + area;
+      break;
   }
 }
