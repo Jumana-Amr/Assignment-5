a2faf77c2da5822a9885bb19769ae0b2ec5ea3fe
JumanaAmr <jumana.amr@ocsbstudent.ca>
2021-02-23 15:21:08 +0000

    Trial 2

--- script.js
+++ script.js
@@ -1,47 +1,44 @@
-   function updateForm() {
-     document.getElementById("id_inputs_area").hidden = true;
-     document.getElementById("id_inputs_circumference").hidden = true;
-     document.getElementById("id_inputs_both").hidden = true;
-     document.getElementById("id_output_area").hidden = true;
-     document.getElementById("id_output_circumference").hidden = true;
-     
-     let shape = document.getElementById("id_shapes") .value;
-     switch (shape) {
-       case "area":
-       document.getElementById("id_inputs_area").hidden = false;
-       break;
-       case "circumference":
-       document.getElementById("id_inputs_circumference").hidden = false;
-       break;
-       case "both":
-       document.getElementById("id_inputs_both").hidden = false;
-       break;
-        }
-    }
-    
-    function calculateAreacalculateCircumfrance() {
-      let shape = document.getElementById("id_shapes").value;
-      let area = 0;
-      let circumference = 0
-      let diameter = document.getElementById("id_diameter").value;
-      switch (shape) {
-        case "area":
-        document.getElementById("id_output_area").hidden = false;
-        area = Math.PI * 1/4 * diameter * diameter;
-        document.getElementById("id_output_area").innerHTML = "Area = " + area;
-        break;
-        case "circumference":
-        document.getElementById("id_output_circumference").hidden = false;
-        circumference = Math.PI * diameter;
-        document.getElementById("id_output_circumference").innerHTML = "Circumference = " + circumference;
-        break;
+function updateForm () {
+  document.getElementById('id-inputs-area').hidden = true
+  document.getElementById('id-inputs-circumference').hidden = true
+  document.getElementById('id-inputs-both').hidden = true
+  document.getElementById('id-output-area').hidden = true
+  document.getElementById('id-output-circumference').hidden = true
+  let const = document.getElementById('id-shapes').value
+  switch (shape) {
+    case "area":
+      document.getElementById('id-inputs-area').hidden = false
+    break
+    case "circumference":
+      document.getElementById('id-inputs-circumference').hidden = false
+    break
+    case "both":
+      document.getElementById('id-inputs-both').hidden = false
+    break
+  }
+}  
+function calculateAreacalculateCircumfrance () {
+  let shape = document.getElementById('id-shapes').value
+  let area = 0
+  let circumference = 0
+  let diameter = document.getElementById('id-diameter').value
+  switch (shape) {
+    case "area":
+      document.getElementById('id-output-area').hidden = false
+      area = Math.PI * 1/4 * diameter * diameter
+      document.getElementById('id-output-area').innerHTML = "Area = " + area
+      break
+    case "circumference":
+        document.getElementById('id-output-circumference').hidden = false
+        circumference = Math.PI * diameter        document.getElementById('id-output-circumference').innerHTML = "Circumference = " + circumference
+        break
         case "both":
-        document.getElementById("id_output_circumference").hidden = false;
-        document.getElementById("id_output_area").hidden = false;
-        area = Math.PI * 1/4 * diameter * diameter;
-        circumference = Math.PI * diameter;
-        document.getElementById("id_output_circumference").innerHTML = "Circumference = " + circumference;
-        document.getElementById("id_output_area").innerHTML = "Area = " + area;
-        break;
-      }
-    }
+        document.getElementById('id-output-circumference').hidden = false
+        document.getElementById('id-output-area').hidden = false
+        area = Math.PI * 1/4 * diameter * diameter
+        circumference = Math.PI * diameter
+        document.getElementById('id-output-circumference').innerHTML = "Circumference = " + circumference
+        document.getElementById('id-output-area').innerHTML = "Area = " + area
+        break
+  }
+}
